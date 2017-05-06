5e62b899007e6190858ed57dfc9671d5b38eb35b
learneman <nuelapia@gmail.com>
2017-05-05 20:49:33 -0700

    adding logout button

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import firebaseApp from '../../globals';
 import {
   Text,
   View,
+  TouchableOpacity
 } from 'react-native';
+import styles from '../../styles/mainstyle'
 
-class settings extends React.Component {
+class Settings extends React.Component {
+  static navigationOptions = {
+    header: null,
+  }
+  async logout() {
+    try {
+      await firebaseApp.auth().signOut();
+      this.props.navigation.navigate('Login');
+    } catch (error) {
+      alert(error);
+    }
+  }
   render() {
     return(
-      <Text>Hello from settings</Text>
+      <View style={{flex: 1}}>
+        <View style={styles.settingsHeader}>
+          <View style={{flex:9.7}}/>
+          <TouchableOpacity onPress={()=> this.logout()}>
+            <View style={styles.smllbutton}>
+              <Text style={styles.buttontext}>Log out</Text>
+            </View>
+          </TouchableOpacity>
+          <View style={{flex:.3}}/>
+        </View>
+        <View style={{flex: 9}}>
+        </View>
+      </View>
     );
   }
-
 }
 
-export default settings;
+export default Settings;
